da1f7947f07641377f794cd37a87f9f53d85f491
gnyandev <gnyandev.patel@anu.edu.in>
2022-05-04 13:07:26 +0530

    User mail connect to API

--- new.js
+++ new.js
@@ -11,6 +11,15 @@ class RandomUser{
     
     static renderUserData(data){
         // console.log(data);
+        let user = data.results[0];
+        let cardElem = document.querySelector('.card');
+        cardElem.innerHTML = `
+        <div class = "card-head">
+            <a href = "mailto:${user.email}"><i class = "fas fa-envelope"></i> Email</a>
+            <div class = "user-image">
+                <img src = "${user.picture.large}" alt = "">
+            </div>
+        </div>
 
         
 document.getElementById('generate-btn').addEventListener('click', () => {
