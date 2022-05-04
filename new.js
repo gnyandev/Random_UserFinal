c490349dce70321688110b88216a82a00639af6a
gnyandev <gnyandev.patel@anu.edu.in>
2022-05-04 12:50:36 +0530

    Adding existing API for fetching user info

---
+++ new.js
@@ -0,0 +1,11 @@
+class RandomUser{
+    constructor(){}
+
+    static fetchFromAPI(){
+        let URL = 'https://randomuser.me/api/';
+        fetch(URL)
+        .then(response => response.json())
+        .then(data => RandomUser.renderUserData(data))
+        .catch(error => alert(error));
+    }
+    
